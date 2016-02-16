9bc32bc3eb58cb07d79b9f812d923e214238993f
Carley Naquin <cnaquin9@yahoo.com>
2016-02-16 17:02:54 -0600

    folder set up

---
+++ PLProject2/11-1/src/Naquin/ui/PersonApp.java
@@ -0,0 +1,11 @@
+package Naquin.ui
+        ;
+
+public class PersonApp {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
